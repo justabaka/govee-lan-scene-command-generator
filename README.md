f676e0d206622555da51230a8066d1dbe0dafd5a
Just a baka <justabaka@gmail.com>
2025-04-15 07:32:40 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Govee LAN Scene Command Generator
+> [!WARNING]
+> LAN Scene control is an undocumented protocol, it may change or break at any time.
+
+Calls the Govee API, retrieves the available scenes and converts them into usable LAN commands so you are no longer vendor locked and actually may switch your Govee scenes using third party software.
+
+## Installation
+Clone the repository (or download the script + txt files manually) and install project requirements:
+* `git clone https://github.com/justabaka/govee-lan-scene-command-generator.git`
+* `pip install -r requirements.txt`
+
+## IoT API and DIY Scenes
+As far as the community found out, command structure may vary from model to model, so it may not work out of the box.
+The easiest way to check if the commands you've generated are valid for your device is calling the IoT API which basically dumps ready commands for you. It's also a great way of obtaining commands for your DIY scenes.
+
+```bash
+# Get token returned by this request
+curl --location 'https://community-api.govee.com/os/v1/login' --header 'Content-Type: application/json' --data '{"email": "email", "password": "password"}'
+# Returns the following JSON: {"message":"Login successful","status":200,"data":{"token":"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"}
+
+# Replace __token__ with an actual token (not including quotation marks)
+curl --location 'https://app2.govee.com/bff-app/v1/exec-plat/home' --header 'Content-Type: application/json' --header 'appVersion: 6.6.30' --header 'Authorization: Bearer __token__'
+```
+
+## Example
+You may use any Govee LAN API command as a template, just make sure you set `"cmd": "ptReal"`.
+```python
+import json
+import socket
+
+LOCAL_IP_ADDRESS = '192.168.0.100'
+CONTROLLER_IP_ADDRESS = '192.168.0.10'
+LISTEN_PORT = 4002
+COMMAND_PORT = 4003
+
+command = { "msg": { "cmd": "ptReal", "data": { "command": ["owABCQIEGgAAAAECAeVqAcgUCu0=", "owEA/TIB////AwCAAAAAACMAAjE=", "owIHAwAB/wUAoBQUAeYKBP///+g=", "owOFzv6m//8I/vARAcgSAeABI7w=", "owQAAgcEAAH/AAPMFBQB5goE/4E=", "owX//1/X/6b//y3+4AEByAIB4G8=", "owYBHQACBQMAAf8AA9gUFAHmCnU=", "owcCZdX/Lf7gAQHIAgHgAgAAAPM=", "o/8AAAAAAAAAAAAAAAAAAAAAAFw=", "MwUEDi8AAAAAAAAAAAAAAAAAABM="] } } }
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+socket.bind((LOCAL_IP_ADDRESS, LISTEN_PORT))
+
+socket.sendto(bytes(json.dumps(command), "utf-8"), (CONTROLLER_IP_ADDRESS, COMMAND_PORT))
+socket.close()
+```
+
+## References
+* https://github.com/egold555/Govee-Reverse-Engineering/issues/11
+* https://github.com/AlgoClaw/Govee/blob/main/decoded/explanation
+* https://github.com/AlgoClaw/Govee/blob/main/decoded/govee_decoded.sh
+* https://disbar.red/note/Govee%20Decoded%202
---
+++ generate_lan_scene_commands.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+import argparse
+import base64
+import json
+import os
+import requests
+from pathlib import PurePath
+
+
+def calculate_checksum(command: str = b''):
+    checksum = 0
+    for i in range(0, len(command)):
+        checksum ^= command[i]
+    return checksum.to_bytes()
+
+
+arg_parser = argparse.ArgumentParser(
+    prog='Govee LAN Scene Command Generator',
+    description='Gets a full list of Govee Scenes that are available for your Govee device via Govee API and converts that into pure LAN command list to call without depending on the Internet of Shit™',
+)
+
+arg_parser.add_argument('-s', '--sku', required=True, help="Your Govee Device SKU (Model). For example, H61F2.")
+arg_parser.add_argument('-a', '--appversion', required=False, default="9999999", help="The desired GoveeHome app version. Default value is 9999999.")
+arg_parser.add_argument('-l', '--load', required=False, default=None, help="Path to a file to load the API response data (JSON) from instead of requesting it via Govee API")
+arg_parser.add_argument('-c', '--cache', required=False, default=False, action='store_true', help="Cache the API response data (JSON) to a file")
+args = arg_parser.parse_args()
+
+base_directory = PurePath(__file__).parent
+if not os.path.exists(base_directory):
+    os.makedirs(base_directory)
+
+if args.load is not None:
+    # Load JSON from a file 
+    load_file = base_directory / args.load
+    print(f"Loading data for SKU '{args.sku}' from file '{load_file}'...")
+    with open(load_file, 'r') as f:
+        data = json.load(f)
+else:
+    # Request the available scenes from the publicly available API
+    print(f"Requesting scenes data for SKU '{args.sku}'...")
+    headers = { "AppVersion": f"{args.appversion}","User-Agent": f"GoveeHome/{args.appversion} (com.ihoment.GoVeeSensor; build:2; iOS 16.5.0) Alamofire/5.6.4"}
+    response = requests.get(f"https://app2.govee.com/appsku/v1/light-effect-libraries?sku={args.sku}", headers=headers)
+    data = json.loads(response.text)
+
+    if args.cache:
+        save_file = base_directory / f"govee_{args.sku}_api_data.json"
+        with open(save_file, 'w') as f:
+            json.dump(data, f)
+        print(f"Saved the API response data as {save_file}.")
+
+
+# Extract only scenes from received data
+scenes = [scene for category in data['data']['categories'] for scene in category['scenes']]
+print(f"Extracted {len(scenes)} scenes.")
+del(data)
+
+new_scenes = {}
+
+# Process data
+for scene in scenes:
+    scene_code = scene["lightEffects"][0]["sceneCode"]
+    
+    # Convert base64 effect value ("scenceParam") to hex values
+    hex_command = base64.b64decode(scene["lightEffects"][0]["scenceParam"].encode('ascii'))
+
+    # hex_command actually consist of multiple subcommands that are sent to the controller.
+    # Count individual commands (or "lines") and convert those values to hex.
+    num_bytes = len(hex_command) + 3 + 13
+    # Bytes that do not fit (division remainder) seem to be discarded.
+    num_commands = num_bytes // 17
+
+    # Add general prefixes and suffixes to the whole command
+    hex_command = b'\x01' + num_commands.to_bytes() + b'\x02' + hex_command + b'\x00' * 13
+
+    # Add subcommand counter prefixes as well as the special last subcommand prefix
+    subcommands = []
+    for i in range(num_commands):
+        if i != num_commands - 1:
+            subcommand_prefix = i.to_bytes()
+        else:
+            subcommand_prefix = b'\xff'
+
+        # Ignore the newly added prefix and read exactly 17 bytes every time
+        start_index = i * 17
+        
+        # Cut the hex_command to num_commands of smaller subcommands and prepend the multiline prefix to them
+        subcommand = b'\xa3' + subcommand_prefix + hex_command[start_index:start_index+17]
+        subcommand += calculate_checksum(subcommand)
+        subcommands.append(base64.b64encode(subcommand).decode('ascii'))
+
+    # Also send a scene code subcommand. We need to flip the byte order here. The prefix here is special, as well as a padding instead of the suffix.
+    scene_code_subcommand = b'\x33\x05\x04' + scene_code.to_bytes(2, byteorder='little') + b'\x00' * 14
+    scene_code_subcommand += calculate_checksum(scene_code_subcommand)
+    subcommands.append(base64.b64encode(scene_code_subcommand).decode('ascii'))
+
+    # Assemble the final pretty dictionary
+    new_scenes[scene["sceneName"]] = {
+        'scene_code': scene_code,
+        'command': subcommands,
+    }
+
+del(scenes)
+
+save_file = base_directory / f"govee_{args.sku}_scenes.json"
+with open(save_file, 'w') as f:
+    json.dump(new_scenes, f)
+print(f"Saved the final JSON data to {save_file}.")
